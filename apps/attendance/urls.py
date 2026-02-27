94140afefca9a9f79ef6a26e319c01390122c5d3
Soumyaranjan63 <sranjan6370@gmail.com>
2026-02-27 09:58:18 +0000

    feat(attendance): add attendance APIs and routing

---
+++ apps/attendance/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from apps.attendance.views import AttendanceViewSet
+
+
+router = DefaultRouter()
+router.register(r"", AttendanceViewSet, basename="attendance")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
---
+++ apps/attendance/views/attendance_views.py
@@ -0,0 +1,34 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from apps.attendance.models import Attendance
+from apps.attendance.selectors.attendance_selector import get_attendance_by_employee, get_all_attendance
+from apps.attendance.serializers.attendance_serializer import AttendanceSerializer
+from apps.attendance.services.attendance_service import create_attendance, delete_attendance
+
+
+class AttendanceViewSet(ModelViewSet):
+    queryset = Attendance.objects.all()
+    serializer_class = AttendanceSerializer
+
+    def list(self, request, *args, **kwargs):
+        employee_id = request.query_params.get("employee_id")
+        if employee_id is not None:
+            attendances = get_attendance_by_employee(employee_id=employee_id)
+        else:
+            attendances = get_all_attendance()
+        serializer = self.get_serializer(attendances, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        attendance = create_attendance(request.data)
+        serializer = self.get_serializer(attendance)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def destroy(self, request, *args, **kwargs):
+        attendance_id = kwargs.get(self.lookup_field)
+        delete_attendance(attendance_id)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
