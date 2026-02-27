e5a34a85cb10ef3e694f85a868583ab57e898ef6
Soumyaranjan63 <sranjan6370@gmail.com>
2026-02-27 14:29:26 +0000

    fix STATIC_ROOT for render

--- config/settings.py
+++ config/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 CORS_ALLOW_ALL_ORIGINS = True
